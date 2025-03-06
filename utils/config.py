63923244b3f2babe54b5b8285ee0a835d706de30
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-03-06 18:22:28 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,9 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
-NAME = "Olympus"
-server = "https://discord.com/invite/odx"
-ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
-OWNER_IDS = [1328025198758461440, 1070619070468214824]
-BotName = "Olympus"
-serverLink = "https://discord.com/invite/odx"
+NAME = "Kranton"
+server = "https://discord.gg/hjSV93j93j"
+ch = "https://discord.com/channels/1343424929269289062/1343520136002535486"
+OWNER_IDS = [1328025198758461440]
+BotName = "Kranton"
+serverLink = "https://discord.gg/hjSV93j93j"
