287ee1abc19e785902a10e0b0c703d6fad248048
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-03-06 21:29:44 +0530

    Update blacklist.py

--- cogs/commands/blacklist.py
+++ cogs/commands/blacklist.py
@@ -25,7 +25,7 @@ class BlacklistWordPaginator:
 
             embed.set_footer(
                 text=f'Page {i // self.per_page + 1}/{total_pages} | Users having Administrator can use Blacklisted Word',
-                icon_url="https://cdn.discordapp.com/avatars/1144179659735572640/a_f061e6472786781e23bac32fa8d0a667.png?width=115&height=115"
+                icon_url="https://cdn.discordapp.com/attachments/1345069871351857265/1347234051420979323/pixelcut-export_1.png?ex=67cb14fc&is=67c9c37c&hm=b64dca9b4377c4a5375931f950fc7296ce5835739076809e8f1e3a997ff73a21&?width=115&height=115"
             )
             pages.append(embed)
 
@@ -211,7 +211,7 @@ class Blacklist(commands.Cog):
             await ctx.reply("The blacklist is full. Maximum 30 words allowed.")
             return
         if await self.is_word_blacklisted(guild_id, word.lower()):
-            embed = discord.Embed(title="<:olympus_notify:1227866804630720565> Access Denied",
+            embed = discord.Embed(title="<:icons_error:1345041194467721327> Access Denied",
                 description=f"`{word}` is already in the blacklist.",
                 color=discord.Color.from_rgb(0, 0, 0)
             )
@@ -219,7 +219,7 @@ class Blacklist(commands.Cog):
             return
 
         await self.add_word_to_blacklist(guild_id, word.lower())
-        embed = discord.Embed(title="<:olympus_tick:1227866641027698792> Success",
+        embed = discord.Embed(title="<:tick_icons:1345041197483298856> Success",
             description=f"Added `{word}` to the blacklist.",
             color=discord.Color.from_rgb(0, 0, 0)
         )
@@ -233,7 +233,7 @@ class Blacklist(commands.Cog):
     async def remove(self, ctx, word: str):
         guild_id = str(ctx.guild.id)
         if not await self.is_word_blacklisted(guild_id, word.lower()):
-            embed = discord.Embed(title="<:olympus_cross:1227866668152393789> Error",
+            embed = discord.Embed(title="<:icon_cross:1345041135156072541> Error",
                 description=f"`{word}` is not in the blacklist.",
                 color=discord.Color.from_rgb(0, 0, 0)
             )
@@ -241,7 +241,7 @@ class Blacklist(commands.Cog):
             return
 
         await self.remove_word_from_blacklist(guild_id, word.lower())
-        embed = discord.Embed(title="<:olympus_tick:1227866641027698792> Success",
+        embed = discord.Embed(title="<:tick_icons:1345041197483298856> Success",
             description=f"Removed `{word}` from the blacklist.",
             color=discord.Color.from_rgb(0, 0, 0)
         )
@@ -257,7 +257,7 @@ class Blacklist(commands.Cog):
         words = await self.get_blacklisted_words(guild_id)
 
         if not words:
-            embed = discord.Embed(title="<:olympus_cross:1227866668152393789> Error",
+            embed = discord.Embed(title="<:icon_cross:1345041135156072541> Error",
                 description="No words are currently blacklisted.",
                 color=discord.Color.from_rgb(0, 0, 0)
             )
@@ -266,7 +266,7 @@ class Blacklist(commands.Cog):
 
         await self.remove_all_words_from_blacklist(guild_id)
 
-        embed = discord.Embed(title="<:olympus_tick:1227866641027698792> Success",
+        embed = discord.Embed(title="<:tick_icons:1345041197483298856> Success",
             description="Cleared all blacklisted words.",
             color=discord.Color.from_rgb(0, 0, 0)
         )
@@ -282,7 +282,7 @@ class Blacklist(commands.Cog):
         guild_id = str(ctx.guild.id)
         words = await self.get_blacklisted_words(guild_id)
         if not words:
-            embed = discord.Embed(title="<:olympus_cross:1227866668152393789> Error",
+            embed = discord.Embed(title="<:icon_cross:1345041135156072541> Error",
                 description="No words are currently blacklisted.",
                 color=discord.Color.from_rgb(0, 0, 0)
             )
@@ -327,13 +327,13 @@ class Blacklist(commands.Cog):
                 return
             if await self.is_user_bypassed(guild_id, target.id):
                 embed = discord.Embed(
-                    description=f"<:olympus_cross:1227866668152393789> | `{target}` is already bypassed.",
+                    description=f"<:icon_cross:1345041135156072541> | `{target}` is already bypassed.",
                     color=discord.Color.from_rgb(0, 0, 0)
                 )
                 await ctx.reply(embed=embed)
                 return
             await self.add_user_to_bypass(guild_id, target.id)
-            embed = discord.Embed(title="<:olympus_tick:1227866641027698792> Success",
+            embed = discord.Embed(title="<:tick_icons:1345041197483298856> Success",
                 description=f"Added `{target}` to the bypass list.",
                 color=discord.Color.from_rgb(0, 0, 0)
             )
@@ -344,14 +344,14 @@ class Blacklist(commands.Cog):
                 await ctx.reply("The bypass list for roles is full. Maximum 30 roles allowed.")
                 return
             if await self.is_role_bypassed(guild_id, target.id):
-                embed = discord.Embed(title="<:olympus_cross:1227866668152393789> Error",
+                embed = discord.Embed(title="<:icons_error:1345041194467721327> Error",
                     description=f"`{target}` is already bypassed.",
                     color=discord.Color.from_rgb(0, 0, 0)
                 )
                 await ctx.reply(embed=embed)
                 return
             await self.add_role_to_bypass(guild_id, target.id)
-            embed = discord.Embed(title="<:olympus_tick:1227866641027698792> Success",
+            embed = discord.Embed(title="<:tick_icons:1345041197483298856> Success",
                 description=f"Added `{target}` to the bypass list.",
                 color=discord.Color.from_rgb(0, 0, 0)
             )
@@ -368,14 +368,14 @@ class Blacklist(commands.Cog):
         guild_id = str(ctx.guild.id)
         if isinstance(target, discord.Member):
             if not await self.is_user_bypassed(guild_id, target.id):
-                embed = discord.Embed(title="<:olympus_cross:1227866668152393789> Error",
+                embed = discord.Embed(title="<:icon_cross:1345041135156072541> Error",
                     description=f"`{target}` is not bypassed.",
                     color=discord.Color.from_rgb(0, 0, 0)
                 )
                 await ctx.reply(embed=embed)
                 return
             await self.remove_user_from_bypass(guild_id, target.id)
-            embed = discord.Embed(title="<:olympus_tick:1227866641027698792> Success",
+            embed = discord.Embed(title="<:tick_icons:1345041197483298856> Success",
                 description=f"Removed `{target}` from the bypass list.",
                 color=discord.Color.from_rgb(0, 0, 0)
             )
@@ -383,14 +383,14 @@ class Blacklist(commands.Cog):
 
         elif isinstance(target, discord.Role):
             if not await self.is_role_bypassed(guild_id, target.id):
-                embed = discord.Embed(title="<:olympus_cross:1227866668152393789> Error",
+                embed = discord.Embed(title="<:icon_cross:1345041135156072541> Error",
                     description=f"`{target}` is not bypassed.",
                     color=discord.Color.from_rgb(0, 0, 0)
                 )
                 await ctx.reply(embed=embed)
                 return
             await self.remove_role_from_bypass(guild_id, target.id)
-            embed = discord.Embed(title="<:olympus_tick:1227866641027698792> Success",
+            embed = discord.Embed(title="<:tick_icons:1345041197483298856> Success",
                 description=f"Removed `{target}` from the bypass list.",
                 color=discord.Color.from_rgb(0, 0, 0)
             )
@@ -407,7 +407,7 @@ class Blacklist(commands.Cog):
         roles = await self.get_bypassed_roles(guild_id)
 
         if not users and not roles:
-            embed = discord.Embed(title="<:olympus_cross:1227866668152393789> Error",
+            embed = discord.Embed(title="<:icon_cross:1345041135156072541> Error",
                 description="No users or roles are currently bypassed.",
                 color=discord.Color.from_rgb(0, 0, 0)
             )
@@ -440,7 +440,7 @@ class Blacklist(commands.Cog):
         if isinstance(error, commands.CommandError):
             if not isinstance(error, commands.CommandOnCooldown):
                 embed = discord.Embed(
-                    description="<:olympus_cross:1227866668152393789> | An error occurred while processing the command. Make sure you have **Administrator** permissios.",
+                    description="<:icon_cross:1345041135156072541> | An error occurred while processing the command. Make sure you have **Administrator** permissios.",
                     color=discord.Color.from_rgb(0, 0, 0)
                 )
                 await ctx.reply(embed=embed)
